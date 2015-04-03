53d3928d3def172f57f6f7fd9ff00df19a125d22
c5sire <rsimon64@gmail.com>
2015-04-03 14:14:01 -0500

    Initial transfer

---
+++ inst/hidap/global.R
@@ -0,0 +1,44 @@
+# for debugging
+# options(warn=0)
+# options(shiny.trace = TRUE)
+# options(warn=2)
+# options(shiny.error=recover)
+app_dir <- "hidap"
+
+pkgs_cran <- c("car", "gridExtra", "GPArotation", "psych", "wordcloud",
+               "AlgDesign", "knitr", "lubridate", "ggplot2", "ggdendro",
+               "shiny", "pryr", "shiny", "magrittr", "tidyr", "dplyr",
+               "broom", "htmlwidgets")
+pkgs_gh <- c("shinyAce","rpivotTable")
+pkgs <- c(pkgs_cran, pkgs_gh)
+
+if(Sys.getenv('SHINY_PORT') == "") {
+
+  running_local <- TRUE
+
+  # no limit to filesize locally
+  options(shiny.maxRequestSize=-1)
+
+  # Windows or Mac
+  if (.Platform$OS.type == 'windows') {
+    Sys.setlocale(category = 'LC_ALL','English_United States.1252')
+  } else {
+    Sys.setlocale(category = 'LC_ALL','en_US.UTF-8')
+  }
+
+  # if radiant package was not loaded load dependencies
+  if(!"package:radiant" %in% search())
+    source(file.path("..",app_dir,"dependencies.R"), local = TRUE)
+
+} else {
+  running_local <- FALSE
+  # limit upload filesize on server (5MB)
+  options(shiny.maxRequestSize=5*1024^2)
+  source(file.path("..",app_dir ,"dependencies.R"), local = TRUE)
+}
+
+# adding the figures path to avoid making a copy of all figures in www/figures
+if(!"package:radiant" %in% search()) {
+  addResourcePath("figures", file.path("..",app_dir,"tools/help/figures/"))
+  addResourcePath("imgs",    file.path("..",app_dir,"www/imgs/"))
+}
